2261def6c70dd7fa846f405659b44d524aa91a4a
simranjit-2101871 <117904538+simranjit-2101871@users.noreply.github.com>
2024-12-25 17:58:38 +0530

    Add files via upload

---
+++ circular_queue.cpp
@@ -0,0 +1,163 @@
+/*#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+struct circularQueue
+{
+    int size;
+    int f;
+    int r;
+    int* arr;
+};
+ 
+ 
+int isEmpty(struct circularQueue *q){
+    if(q->r==q->f){
+        return 1;
+    }
+    return 0;
+}
+ 
+int isFull(struct circularQueue *q){
+    if((q->r+1)%q->size == q->f){
+        return 1;
+    }
+    return 0;
+}
+ 
+void enqueue(struct circularQueue *q, int val){
+    if(isFull(q)){
+        cout<<"The queue is full"<<endl;
+    }
+    else{
+        q->r = (q->r +1)%q->size;
+        q->arr[q->r] = val; 
+        cout<<"The enqueued element:"<<val<<endl;
+    }
+}
+ 
+int dequeue(struct circularQueue *q){
+    int a = -1;
+    if(isEmpty(q)){
+        cout<<"The queue is empty"<<endl;
+    }
+    else{
+        q->f = (q->f +1)%q->size;
+        a = q->arr[q->f]; 
+    }
+    return a;
+}
+ 
+ 
+int main(){
+    struct circularQueue q;
+    q.size = 4;
+    q.f = q.r = 0;
+    q.arr = (int*) malloc(q.size*sizeof(int));
+    
+    // Enqueue few elements
+    enqueue(&q, 12);
+    enqueue(&q, 15);
+    enqueue(&q, 1); 
+    cout<<"Dqueuing element:"<< dequeue(&q)<<endl;
+    cout<<"Dequeuing element:"<< dequeue(&q)<<endl;
+    cout<<"Dequeuing element:"<< dequeue(&q)<<endl; 
+    enqueue(&q, 45);
+    enqueue(&q, 45);
+    enqueue(&q, 45);
+ 
+    if(isEmpty(&q)){
+        cout<<"The queue is empty"<<endl;
+    }
+    if(isFull(&q)){
+        cout<<"The queue is full"<<endl;
+    }
+ 
+    return 0;
+}
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// circular queue using class
+
+/*#include<iostream>
+using namespace std;
+class CircularQueue{
+    int *arr;
+    int front;
+    int rear;
+    int size;
+    
+    public:
+    // Initialize your data structure.
+    CircularQueue(int n){
+        size = n;
+        arr = new int[size];
+        front = rear = -1;
+    }
+
+    // Enqueues 'X' into the queue. Returns true if it gets pushed into the stack, and false otherwise.
+    bool enqueue(int value){
+        //to check whther queue is full
+        if( (front == 0 && rear == size-1) || (rear == (front-1)%(size-1) ) ) {
+            //cout << "Queue is Full";
+            return false;
+        }
+        else if(front == -1) //first element to push
+        {
+			front = rear = 0;
+            
+        }
+        else if(rear == size-1 && front != 0) {
+            rear = 0; //to maintain cyclic nature
+        }
+        else
+        {//normal flow
+            rear++;
+        }
+        //push inside the queue
+        arr[rear] = value;
+        
+        return true;
+    }
+
+    // Dequeues top element from queue. Returns -1 if the stack is empty, otherwise returns the popped element.
+    int dequeue(){
+        if(front == -1){//to check queue is empty
+            //cout << "Queue is Empty " << endl;
+            return -1;
+        }
+        int ans = arr[front];
+        arr[front] = -1;
+        if(front == rear) { //single element is present
+            front = rear = -1;
+        }
+        else if(front == size - 1) {
+            front = 0; //to maintain cyclic nature
+        }
+        else
+        {//normal flow
+            front++;
+        }
+        return ans;
+    }
+};
+*/
+
+
+
